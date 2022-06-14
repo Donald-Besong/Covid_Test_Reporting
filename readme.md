932d9c588d305861e5881fd3d1f1f27d1ea3442a
Donald Besong <donald.besong.principles@gmail.com>
2022-06-14 19:09:07 +0100

    initial commit

---
+++ readme.md
@@ -0,0 +1,87 @@
+<div id="top"></div>
+<!--
+*** Thanks for checking out my project
+*** Dr Donald O. Besong
+-->
+
+
+<!-- PROJECT LOGO -->
+<br />
+<div align="center">
+  <a href="http://github.com/Donald-Besong">
+    <img src="src/data/images/logo.png" alt="Logo" width="80" height="80">
+  </a>
+
+<h3 align="center">Donald's Covid test Calculations</h3>
+
+  <p align="center">
+    project_description
+    <br />
+    <a href="http://github.com/Donald-Besong/Covid_Test"><strong>Explore the docs »</strong></a>
+  </p>
+</div>
+
+
+
+
+## How to test the program
+<p>
+Assume that you would like to travel, and you have been sent a test kit. 
+You will not be allowed to travel unless your result is
+negative. The test kit contains eight test strips.
+</p>
+The id's of your strips are shown on your test strips. Please understand that
+these are supposed to be valid id's from a huge database with millions of id's, but in this prototyoe
+it is just my small data file <b>/src/data/data.xlsx</b>. My file has a total
+of 60 numbers from x000001 to x000060. Therefore, you can use any of the numbers 
+as your test strip. 
+<p>
+There are two kinds of test I would like you to do. 
+
+1. Testing whether the code actually does what it is intended to do: 
+For this purpose you have to look at the data file and select a stip 
+id from any sheet (or group) S1, S2, or S3.
+Please also take note of the value that results in positive or negative for 
+strips in their respective sheets.
+Then run the test as described in the instructions below, with those values. Finally, compare
+the result from the program with the corresponding spreadsheet.
+Please remember that the values are simply a representation of the lines
+you would see on your test trip after the swab 
+and test (please refer to the manuscript for details).
+
+2. Investigating your rate  of success in faking a desired result. 
+For this purpose, do not look at the data file! Select a stip 
+id (x000001 to x000060), without a clue as to which sheet (or group) it belongs.
+Please also guess a value that you wish results in a negative.
+Then run the test as described below, with those values.
+Keep chosing different strips and testing. In the end, your score
+as to how successful you are at cheating will be calculated.
+Please do not use the same strip id more than once,
+because in real life you only use a strip once(please refer to the publication).
+In real life, once a candidate's result is calculated from a strip, that strip
+should be deleted from the database. In this prototype, I do not delete, for simplicity.
+So please do not reus any strips.
+</p>
+           
+
+
+## Instructions
+
+1. Clone the repository
+2. Make sure that in your command line, you are in the root folder, which contains
+   requirements.txt, and all the code.
+3. virtualenv -p python3 cov_env3
+
+4. Activate the virtual environment  . cov_env3/bin/activate
+
+5. Install python packages in requirements.txt using pip in your python3
+environment   
+  
+
+5. Run the following command, changing the argument values:
+<p align="center"> python3 src/main_program.py --which_test=test1 --name=Jane --strip_id=x000001 --value=101 </p>
+<br>
+--which_test=test1 if you need the first test listed in "How to test the program",
+--which_test=test2 for the second test. See "How to test the program".
+<br>
+Please replace with your name, strip number and test result.
---
+++ src/main_program.py
@@ -0,0 +1,142 @@
+import iterdata
+import main_argparser
+import argparse
+from datetime import datetime, date
+from itertools import tee
+import pandas as pd
+import copy
+import numpy as np
+pd.options.mode.chained_assignment = None
+import math
+
+#*****************begin reading arguments
+data_source = main_argparser.data_source
+name = main_argparser.name
+strip_id = main_argparser.strip_id
+value = main_argparser.value
+which_test = main_argparser.which_test  
+#*****************end reading arguments
+
+x = iterdata.IterData(data_source)
+x.iterRead()
+dataSet1 = x._dataSet1
+dataSet2 = x._dataSet2
+dataSet3 = x._dataSet3
+
+message = "hello there"
+dataSet1Rows = dataSet1.rows
+dataSet2Rows = dataSet2.rows
+dataSet3Rows = dataSet3.rows
+	
+def get_length(x):
+	i = 0
+	row_data = next(x, None)
+	while row_data:
+		row = [cell.value for cell in row_data]
+		if not row[0] and row[0] != 0:
+			return(i)
+		row_data = next(x, None)
+		i += 1
+	return(i)
+
+dataSet1Rows_glob, dataSet1Rows_d = tee(dataSet1Rows)
+d1 = get_length(dataSet1Rows_d)
+		
+dataSet2Rows_glob, dataSet2Rows_d = tee(dataSet2Rows)
+d2 = get_length(dataSet2Rows_d)	
+
+dataSet3Rows_glob, dataSet3Rows_d = tee(dataSet3Rows)
+d3 = get_length(dataSet3Rows_d)
+
+def createList(data, n):
+	row_data = next(data, None)[0].value
+	data_list = []
+	for i in range(1, n):
+		row_data = next(data, None)[0].value
+		data_list.append(row_data)
+	return data_list
+
+Set1 = createList(dataSet1Rows_glob, d1)
+Set2 = createList(dataSet2Rows_glob, d2)
+Set3 = createList(dataSet3Rows_glob, d3)
+			
+def covidResult(strip_id, v):
+    result = ""
+    success = 0
+    if strip_id in Set1:
+        print(f'Hello {name}. Your strip id was {strip_id}, and your value is {v} in S1.')
+        if v == '100':
+            success = success + 1
+            result = "Your result is negative."
+        elif v == '110':
+            result = "Your result is positive."
+        else:
+            result = "undefined - please repeat test."
+    elif strip_id in Set2:
+        print(f'Hello {name}. Your strip id was {strip_id}, and your value is {v} in S2.')
+        if v == '010':
+            success = success + 1
+            result = "Your result negative."
+        elif v == '011':
+            result = "Your result positive."
+        else:
+            result = "undefined - please repeat test"
+    elif strip_id in Set3:
+        print(f'Hello {name}. Your strip id was {strip_id}, and your value is {v} in S3.')
+        if v == '001':
+            success = success + 1
+            result = "Your result negative."
+        elif v == '101':
+            result = "Your result positive."
+        else:
+            result = "undefined - please repeat test"
+    else:
+        result = "The strip id is invalid"
+    return result
+    
+			
+def testCheater(strip_id, v):
+    success = 0
+    total = 1
+    if strip_id in Set1:
+        if v == '100':
+            success = 1
+    elif strip_id in Set2:
+        if v == '010':
+            success = 1
+    elif strip_id in Set3:
+        if v == '001':
+            success = 1
+    return success, total
+    	    
+if __name__ == '__main__':
+    success = 0
+    total = 0
+    if which_test == "test1":
+        test = covidResult(strip_id, value)
+        print(test)
+    elif which_test == "test2":
+        n = input("Enter the number of trials required:")
+        while total < int(n):
+            strip_id = input("Enter strip id:")
+            value = input("Enter value:")
+            output = testCheater(strip_id, value)
+            success = success + output[0]
+            total = total + output[1]
+        print('\n ****************************')
+        print(f'total={total} and success={success}')
+        print('****************************')
+    else:
+        print("Sorry, we do not offer the requested test.")
+        
+    
+
+    
+		
+		
+
+    
+
+
+
+
